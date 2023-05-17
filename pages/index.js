c5f06af6f46042cfa41096b07a3c6cba9d948e1c
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-05-17 19:05:43 +0530

    creating navbar and footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,11 @@ export default function Home() {
         />
         <link rel="icon" href="/logo2.png" />
       </Head>
-      <div className="bg-red-300 ">hi</div>
+      <Navbar />
+      <div className="w-full h-full">
+        <img src="/home.jpg" alt="home" />
+      </div>
+      <Footer />
     </>
   );
 }
