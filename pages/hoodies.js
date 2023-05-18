4f60d98ddd1d868beaacc5925b06eed653d59f3d
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-05-18 21:13:25 +0530

    hoodie and mugs page done

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -1,7 +1,177 @@
+import Link from "next/link";
 import React from "react";
 
 const hoodies = () => {
-  return <div>hoodies</div>;
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer  shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto  "
+                    src="https://m.media-amazon.com/images/I/51ZL6kAawTL._UL1200_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center ">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/51ZL6kAawTL._UL1200_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/51ZL6kAawTL._UL1200_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/51ZL6kAawTL._UL1200_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/412yyn31AzL._UX679_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/412yyn31AzL._UX679_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/412yyn31AzL._UX679_.jpg "
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/412yyn31AzL._UX679_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Hoodie
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default hoodies;
--- pages/mugs.js
+++ pages/mugs.js
@@ -1,7 +1,177 @@
+import Link from "next/link";
 import React from "react";
 
 const mugs = () => {
-  return <div>mugs</div>;
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer  shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto  "
+                    src="https://m.media-amazon.com/images/I/61xSIru3MZL._SL1500_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center ">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61xSIru3MZL._SL1500_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61xSIru3MZL._SL1500_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61xSIru3MZL._SL1500_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61gHbn4CgWL._SL1080_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61gHbn4CgWL._SL1080_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61gHbn4CgWL._SL1080_.jpg "
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/product/nextstore"} legacyBehavior>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="h-[40vh] block m-auto"
+                    src="https://m.media-amazon.com/images/I/61gHbn4CgWL._SL1080_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Mugs
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    "Shop Smart. Shop Next"
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default mugs;
